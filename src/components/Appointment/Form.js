1951ee378a294f2f1606d577349df873c2667579
Alice Lin <alicexxlin@gmail.com>
2023-07-19 21:17:27 -0600

    implement test files

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -25,7 +25,7 @@ export default function Form(props) {
       return;
     }
 
-    if (!interviewer) {
+    if (interviewer === null) {
       setError('Please select an interviewer');
       return;
     }
